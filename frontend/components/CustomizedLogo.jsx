fb82d7541ce7046d7400186f75df7ca6625da11b
Alyce Barreto <malycebarreto@gmail.com>
2025-01-21 20:51:37 -0300

    ajusta logo

---
+++ frontend/components/CustomizedLogo.jsx
@@ -0,0 +1,7 @@
+export const CustomizedLogo = () => {
+    return (
+      <div className="rounded-full bg-primary w-32 h-32 flex items-center justify-center">
+        <img src="/logo.png" alt="Logo" className="w-32 h-32" />
+      </div>
+    )
+  }
